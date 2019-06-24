b23e64430ad66569ca90ffc421f893e514bd7d01
Vincent Smeets <Vincent.VSmeets@GMail.com>
2019-06-24 20:03:22 +0200

    service/fileimporter: Added the configuration.

---
+++ service/fileimporter/src/main/java/nl/vsmeets/amr/service/fileimporter/ServiceFileimporterConfig.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2019 Vincent Smeets
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package nl.vsmeets.amr.service.fileimporter;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import nl.vsmeets.amr.service.fileimporter.beans.FileImporterBean;
+import nl.vsmeets.amr.service.p1telegram.reader.ServiceP1TelegramReaderConfig;
+
+/**
+ * The configuration class for file importer service.
+ *
+ * @author vincent
+ */
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude = {})
+@Import({
+    // Components in this module.
+    FileImporterBean.class, //
+    // Other modules.
+    ServiceP1TelegramReaderConfig.class //
+})
+public class ServiceFileimporterConfig {
+
+}
