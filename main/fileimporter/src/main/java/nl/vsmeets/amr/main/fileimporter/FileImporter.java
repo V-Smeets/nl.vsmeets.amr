0832c507bd39bffacd7abbe60ba786aafb20a6a2
Vincent Smeets <Vincent.VSmeets@GMail.com>
2019-06-24 20:03:53 +0200

    main/fileimporter: Added the main class.

---
+++ main/fileimporter/src/main/java/nl/vsmeets/amr/main/fileimporter/FileImporter.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2019 Vincent Smeets
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package nl.vsmeets.amr.main.fileimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Import;
+
+import nl.vsmeets.amr.service.fileimporter.ServiceFileimporterConfig;
+
+/**
+ * The main entry point to import P1-Telegrams from a file.
+ *
+ * @author vincent
+ */
+@SpringBootConfiguration
+@Import({
+    // Components in this module.
+    // Other modules.
+    ServiceFileimporterConfig.class //
+})
+public class FileImporter {
+
+  /**
+   * The main entry point.
+   *
+   * @param args
+   *        The command line arguments.
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(FileImporter.class, args);
+  }
+
+}
