63ccde4c1533a2e82716adf463219b3a391d7a36
Vincent Smeets <Vincent.VSmeets@GMail.com>
2019-12-29 20:22:10 +0100

    Receive a message in P1TelegramReceiverBean

--- frontend/amqp/src/main/java/nl/vsmeets/amr/frontend/amqp/FrontendAmqpConfig.java
+++ frontend/amqp/src/main/java/nl/vsmeets/amr/frontend/amqp/FrontendAmqpConfig.java
@@ -15,8 +15,26 @@
  */
 package nl.vsmeets.amr.frontend.amqp;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
+
+import lombok.RequiredArgsConstructor;
+import nl.vsmeets.amr.frontend.amqp.beans.FrontendAmqpProperties;
+import nl.vsmeets.amr.frontend.amqp.beans.P1TelegramReceiverBean;
+import nl.vsmeets.amr.frontend.amqp.beans.ServerFatalExceptionStrategy;
 
 /**
  * The configuration class for the AMQP front end.
@@ -25,6 +43,37 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan
+@EnableRabbit
+@RequiredArgsConstructor
 public class FrontendAmqpConfig {
+
+  @Bean
+  public Queue deadLetterQueue(final FrontendAmqpProperties properties) {
+    final String deadLetterQueueName = properties.getDeadLetterQueueName();
+    return QueueBuilder.durable(deadLetterQueueName).lazy().build();
+  }
+
+  @Bean
+  public ErrorHandler errorHandler(final ServerFatalExceptionStrategy exceptionStrategy) {
+    return new ConditionalRejectingErrorHandler(exceptionStrategy);
+  }
+
+  @Bean
+  public SimpleMessageListenerContainer messageListenerContainer(SimpleRabbitListenerContainerFactory factory,
+      @Qualifier("queue") Queue queue, P1TelegramReceiverBean p1TelegramReceiverBean,
+      @Qualifier("errorHandler") ErrorHandler errorHandler) {
+    final SimpleMessageListenerContainer container = factory.createListenerContainer();
+    container.setQueues(queue);
+    container.setMessageListener(p1TelegramReceiverBean);
+    container.setErrorHandler(errorHandler);
+    return container;
+  }
+
+  @Bean
+  public Queue queue(final FrontendAmqpProperties properties) {
+    final String queueName = properties.getQueueName();
+    final String deadLetterQueueName = properties.getDeadLetterQueueName();
+    return QueueBuilder.durable(queueName).deadLetterExchange("").deadLetterRoutingKey(deadLetterQueueName).build();
+  }
 
 }
